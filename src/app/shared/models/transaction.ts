3c28433a64637138ae0fb4255ff2af315e5fd19b
ahadsenja <hamdanilm96@gmail.com>
2021-09-30 11:43:44 +0800

    feat: add jurnal_id field

--- src/app/shared/models/transaction.ts
+++ src/app/shared/models/transaction.ts
@@ -1,8 +1,9 @@
 export class Transaction {
- id: number = 0;
- date: string = '';
- description: string = '';
- debit: number = 0;
- credit: number = 0;
- balance: number = 0;
+  id: number = 0;
+  date: string = '';
+  description: string = '';
+  debit: number = 0;
+  credit: number = 0;
+  balance: number = 0;
+  jurnal_id: number = 0;
 }
