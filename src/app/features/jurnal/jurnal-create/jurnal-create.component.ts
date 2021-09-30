afdd6d8d00ac9cd8753b9de51973533fd4a3637c
ahadsenja <hamdanilm96@gmail.com>
2021-09-30 11:46:31 +0800

    feat: add jurnal create funtion

--- src/app/features/jurnal/jurnal-create/jurnal-create.component.ts
+++ src/app/features/jurnal/jurnal-create/jurnal-create.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+import { Jurnal } from 'src/app/shared/models/jurnal';
+import { JurnalService } from '../../services/jurnal.service';
 
 @Component({
   selector: 'app-jurnal-create',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JurnalCreateComponent implements OnInit {
 
-  constructor() { }
+  formValueGroup = new FormGroup({});
+  isSubmitted = false;
+
+  jurnals: Jurnal[] = [];
+  jurnalObj: Jurnal = new Jurnal();
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private jurnalService: JurnalService
+  ) { }
 
   ngOnInit(): void {
+    this.formValueGroup = this.formBuilder.group({
+      name: [''],
+      description: ['']
+    })
+  }
+
+  onCreateJurnal() {
+    this.jurnalObj.name = this.formValueGroup.value.name;
+    this.jurnalObj.description = this.formValueGroup.value.description;
+
+    this.jurnalService.create(this.jurnalObj).subscribe(data => {
+      console.log('Data jurnal yg baru aja dibuat: ' + data);
+      this.isSubmitted = true;
+    })
   }
 
 }
